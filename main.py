9b8bd57126e5d775617c3b70eaea723205c14edd
MenuaDesign <67699503+MenuaDesign@users.noreply.github.com>
2021-02-09 14:17:13 +0100

    1.0.5

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import discord
 import os
 client = discord.Client()
-@client.event
-async def on_ready():
-    print("Logged {0.user}\nReady".format(client))
-CountLast = 1
+CountLast = 0
 Author_last = None
 Author_current = None
 @client.event
-async def on_message(message):
-    try:
-        global CountLast
-        global Author_last
+async def on_ready():
+    global CountLast
+    global Author_last
+    print("Logged {0.user}\nReady".format(client))
+    channelid = (806265386981916715)
+    object = await client.get_channel(channelid).history(limit=1).flatten()
+    CountLast = int(object[0].content)
+    Author_last = object[0].author.id
+    print(CountLast)
 
-        msg = await message.channel.fetch_message('807740947290128395')
-        print(msg)
-        Author_current = message.author.id
-        channelid = (806265386981916715)
+@client.event
+async def on_message(message):
+    global CountLast
+    global Author_last
+    Author_current = message.author.id
+    channelid = (806265386981916715)
+    try:
         if message.channel.id == channelid:
             if Author_current == Author_last:
+                print("Author can't write again")
                 await message.delete()
                 return
             if int(message.content) == CountLast + 1:
                 print("------------------------")
-                print(CountLast)
+                print(CountLast + 1)
                 print("-----------")
                 CountLast += 1
-                print(CountLast)
+                print(CountLast + 1)
                 print("------------------------")
                 Author_last = message.author.id
                 return
             elif int(message.content) != CountLast + 1:
+                print("Wrong next number")
                 await message.delete()
                 return
     except ValueError:
+        print('Except error Value')
         await message.delete()
 client.run('ODA2MjcwNjcwNDQyMDcwMDU2.YBnAEw.cphNfm4URwKBmeuW9jSbGKbMW4A')
