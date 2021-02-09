1423204917d0fcbb1e76b54c8c2f5504bab3f255
MenuaDesign <67699503+MenuaDesign@users.noreply.github.com>
2021-02-09 12:53:35 +0100

    1.0.5

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Logged {0.user}\nReady".format(client))
-Count = 254
+CountLast = 1
 Author_last = None
 Author_current = None
 @client.event
 async def on_message(message):
     try:
-        global Count
+        global CountLast
         global Author_last
+
+        msg = await message.channel.fetch_message('807740947290128395')
+        print(msg)
         Author_current = message.author.id
         channelid = (806265386981916715)
         if message.channel.id == channelid:
             if Author_current == Author_last:
                 await message.delete()
                 return
-            if int(message.content) == Count + 1:
+            if int(message.content) == CountLast + 1:
                 print("------------------------")
-                print(Count)
+                print(CountLast)
                 print("-----------")
-                Count += 1
-                print(Count)
+                CountLast += 1
+                print(CountLast)
                 print("------------------------")
                 Author_last = message.author.id
                 return
-            elif int(message.content) != Count + 1:
+            elif int(message.content) != CountLast + 1:
                 await message.delete()
                 return
-
     except ValueError:
         await message.delete()
-client.run(os.environ['TOKEN'])
+client.run('ODA2MjcwNjcwNDQyMDcwMDU2.YBnAEw.cphNfm4URwKBmeuW9jSbGKbMW4A')
